443aa908ba9f1ea046275f91178ae4483fc848e2
NailMarkov <markov_nail@mail.ru>
2023-08-21 10:10:07 +0300

    Добавляет реализацию блока "Туры"

--- source/js/main.js
+++ source/js/main.js
@@ -3,7 +3,7 @@ import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 import {initVideos} from './modules/video';
 import {showMenu} from './modules/menu';
-import {initSliderHero} from './modules/swiper';
+import {initSliderHero, initSliderTours} from './modules/swiper';
 import {initAudio} from './modules/audio';
 
 // ---------------------------------
@@ -20,6 +20,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
   showMenu();
   initSliderHero();
+  initSliderTours();
   initVideos();
   initAudio();
 
--- source/js/modules/swiper.js
+++ source/js/modules/swiper.js
@@ -12,3 +12,30 @@ export const initSliderHero = () => {
 
   return swiperHero;
 };
+
+export const initSliderTours = () => {
+  const sliderElement = document.querySelector('[data-slider="tours"]');
+  const swiperTours = new window.Swiper(sliderElement, {
+    navigation: {
+      prevEl: document.querySelector('[data-prev="tours"]'),
+      nextEl: document.querySelector('[data-next="tours"]'),
+    },
+    breakpoints: {
+      320: {
+        initialSlide: 1,
+        slidesPerView: 1,
+      },
+      768: {
+        initialSlide: 1,
+        slidesPerView: 2,
+        spaceBetween: 18,
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+    },
+  });
+
+  return swiperTours;
+};
