da89eec848c0f79734dcd2b109f14bdda19cd656
Nail Markov <markov_nail@mail.ru>
2023-08-29 23:19:52 +0300

    Добавляет стилизацию блока "Контакты"

--- source/js/modules/map.js
+++ source/js/modules/map.js
@@ -1,16 +1,30 @@
-import '../vendor/leaflet';
+import L from '../vendor/leaflet';
 
 export const initMap = () => {
   const mapElement = document.querySelector('[data-map]');
+
   const mapOptions = {
-    center: [55.816176, 37.636437],
-    zoom: 15,
+    scrollWheelZoom: false,
+    center: [55.823325, 37.625415],
+    zoom: 13,
   };
-  const layer = new L.TileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png');
 
-  const createMap = new L.map(mapElement, mapOptions);
+  const iconOptions = L.icon({
+    iconUrl: 'img/svg/map-pin.svg',
+    iconSize: [48, 48],
+    iconAnchor: [24, 48],
+  });
 
-  createMap.addLayer(layer);
+  const map = L.map(mapElement, mapOptions);
 
-  return createMap;
+  L.marker([55.816654, 37.636922], iconOptions).addTo(map);
+
+  L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}', {
+    minZoom: 10,
+    subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+  }).addTo(map);
+
+  mapElement.querySelector('.leaflet-control-attribution').style.visibility = 'hidden';
+
+  return map;
 };
