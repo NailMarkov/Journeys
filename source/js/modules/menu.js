2a53f35c725b8fe7a70ff74aee67ac61764d63af
NailMarkov <markov_nail@mail.ru>
2023-09-12 16:28:28 +0300

    Добавляет баг-лист2(part2)

--- source/js/modules/menu.js
+++ source/js/modules/menu.js
@@ -2,13 +2,24 @@ export const showMenu = () => {
   const navElement = document.querySelector('.page-header__nav');
   const headerListElement = navElement.querySelector('.page-header__list');
   const togglerElement = navElement.querySelector('.page-header__toggle');
-  const wrapperElement = document.querySelector('.wrapper');
+  const bodyElement = document.querySelector('body');
+
+  const hideOverlay = (link) => {
+    link.addEventListener('click', () => {
+      bodyElement.classList.toggle('scroll-lock');
+      navElement.classList.toggle('page-header__nav--opened');
+      headerListElement.classList.toggle('page-header__list--opened');
+      togglerElement.classList.toggle('page-header__toggle--opened');
+    });
+  };
+
+  headerListElement.querySelectorAll('.page-header__link').forEach(hideOverlay);
 
   const menu = togglerElement.addEventListener('click', () => {
     navElement.classList.toggle('page-header__nav--opened');
     headerListElement.classList.toggle('page-header__list--opened');
     togglerElement.classList.toggle('page-header__toggle--opened');
-    wrapperElement.classList.toggle('wrapper--opened');
+    bodyElement.classList.toggle('scroll-lock');
   });
 
   return menu;
